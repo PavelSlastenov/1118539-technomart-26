490f326a44b201e1dc3576b879502791cb6cea58
PavelSlastenov <52205500+PavelSlastenov@users.noreply.github.com>
2019-10-31 22:47:07 +0300

    Внес правки в JS часть 1

--- js/popup.js
+++ js/popup.js
@@ -5,7 +5,7 @@ var popup = document.querySelector(".modal-feedback");
 var close = popup.querySelector(".modal-close");
 
 var form = popup.querySelector("form");
-var name = popup.querySelector("[name=name]"); /*не работает фокус на поле имени при открытии?*/
+var name = document.querySelector('.modal-name')
 var isStorageSupport = true;
 var storage = "";
 try {
@@ -15,46 +15,51 @@ try {
 } catch (err) {
   isStorageSupport = false;
 }
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
-  name.focus();
-  if (storage) {
-    name.value = storage;
-    mail.focus();
-  } else {
-    name.focus();
-  }
-  if (storage) {
-    mail.value = storage;
-    text.focus();
-  } else {
-    mail.focus();
-  }
-});
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-});
-form.addEventListener("submit", function (evt) {
-  if (!name.value || !mail.value) {
-    evt.preventDefault();
-    popup.classList.add("modal-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("login", name.value);
-    }
-  }
-});
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
-    }
-  }
-});
 
+if (link) { //Если я правильно понял, эта команда проверяет есть такая ссылка на странице или нет? Если находит, то начинает выполнять код ниже, я оборачиваю в проверку все команды ниже до самого конца модального окна?
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    name.focus();
+    if (storage) {
+      name.value = storage;
+      mail.focus();
+    } else {
+      name.focus();
+    }
+    if (storage) {
+      mail.value = storage;
+      text.focus();
+    } else {
+      mail.focus();
+    }
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!name.value || !mail.value) {
+      evt.preventDefault();
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", name.value);
+      }
+    }
+  });
+  
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
+}
 
 /*--------------------------------------modal-map------------------------------*/
 
@@ -81,20 +86,22 @@ window.addEventListener("keydown", function (evt) {
 
 /*--------------------------------------modal-catalog-cart---------------------------*/
 
-var cartlink = document.querySelector(".link-price");
-var cartlink = document.querySelector(".link-price-right");
-var cartlink = document.querySelector(".link-price-catalog");
-var cartlink = document.querySelector(".link-price-right-catalog");
+var cartlink = document.querySelectorAll(".buy");
 var cartPopup = document.querySelector(".modal-cart");
 var cartClose = mapPopup.querySelector(".modal-close");
-cartLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.add("modal-show");
+cartLink.forEach(function (element) {
+
+  element.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("modal-show");
+  })
 });
+
 cartClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   cartPopup.classList.remove("modal-show");
 });
+
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (cartPopup.classList.contains("modal-show")) {
